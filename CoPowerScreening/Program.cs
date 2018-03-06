870056ad9bf62ec23500ad68d4767aa9b2a5c377
Adam <spidermain50@gmail.com>
2018-03-06 16:22:38 -0500

    Add project files.

---
+++ CoPowerScreening/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CoPowerScreening
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
